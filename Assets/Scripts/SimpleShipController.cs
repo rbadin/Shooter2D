6beb472c4f3510ea53798d7bfd59fde131abfb12
rodrigobadin <r.badin@gmail.com>
2017-02-08 14:45:00 -0200

    Chapter 02.01

---
+++ Assets/Scripts/SimpleShipController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class SimpleShipController : MonoBehaviour
+{
+
+    private void Awake()
+    {
+
+    }
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        transform.Translate(0, y, 0);
+        transform.Rotate(0, 0, -x);
+    }
+}
